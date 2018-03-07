9bfa06868cf91358e0378caf1540a218f1a7b777
nunocartier <soarestomas@yahoo.com.br>
2018-03-07 15:48:56 +0000

    dealers added

---
+++ dealers/src/main/App.java
@@ -0,0 +1,10 @@
+package dealers.src.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class App{
+  public static void main(String[] args){
+    stringApplication.run(Application.class, args);
+  }
+}
